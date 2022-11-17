c0afd64183bc0cbf21741f0e78034d6ca22af539
Your Name <you@example.com>
2022-11-17 11:55:41 -0600

    weblab12

--- student-sign-in-client/src/App.vue
+++ student-sign-in-client/src/App.vue
@@ -46,7 +46,7 @@ export default {
       })
       .catch( err => {
         let msg = err.response.data.join(',')
-        alert('Error adding student\n' + msg)
+        alert('Error adding student. Star ID must be unique.')
       })
     },
     studentArrivedOrLeft(student, present){
